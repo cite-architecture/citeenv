2f5de456df6dddde701ccad73956a2eb53d956f3
Neel Smith <dnsmith.neel@gmail.com>
2017-03-08 09:21:54 +0100

    Add first placeholder file

---
+++ shared/src/main/scala/citeenv/CiteEnvironment.scala
@@ -0,0 +1,7 @@
+package edu.holycross.shot.citeenv
+
+import edu.holycross.shot.cite._
+import edu.holycross.shot.ohco2._
+import edu.holycross.shot.citeobj._
+
+case class CiteEnvironment(label: String)
